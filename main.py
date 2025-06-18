604995df485988b222682980d44354d588b02d58
RubenSab <rubensabatini2005@gmail.com>
2025-06-18 22:26:29 +0200

    test

--- main.py
+++ main.py
@@ -39,5 +39,5 @@ DictToHtml(
     'it', 'ANSA.it AI Overview',
     'https://rubensabatini.com', 'by Ruben Sabatini',
     f'Riassunto delle notizie di Ansa.it di oggi {today_date} fatto da Google Gemini',
-    'docs/index.html', '../style.css'
+    'docs/index.html', 'docs/style.css'
 ).generate_html()
